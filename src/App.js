28ae68cc23483d9b98b625f834a0d8b8ae950fe5
giovannicirrincione <giovannicirrincione22@gmail.com>
2024-09-30 11:56:09 -0300

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>¡Hola Mundo!</h1>
+        <p>
+          Esta es una página sencilla en React para probar despliegue en la nube.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
